e7e903a02090f80536d8cb6baf9481f281ca006e
Ayush Ranjan <ranaayush0725@gmail.com>
2024-03-28 23:48:44 +0530

    new commit

---
+++ lib/SplashScreen/splashScreen.dart
@@ -0,0 +1,51 @@
+import 'dart:async';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:she_secure/HomeScreen/homeScreen.dart';
+import 'package:she_secure/LoginScreen/login_screen.dart';
+import 'package:she_secure/WelcomeScreen/welcomeScreen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  startTimer() async {
+    Timer(const Duration(seconds: 3), () {
+      if (FirebaseAuth.instance.currentUser != null) {
+        Navigator.of(context).pushReplacement(
+            MaterialPageRoute(builder: (context) => const HomeScreen()));
+      } else {
+        Navigator.of(context).pushReplacement(
+            MaterialPageRoute(builder: (context) => const LoginScreen()));
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    startTimer();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Container(
+        color: Colors.white,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.asset('assets/images/splash.webp',
+                height: MediaQuery.of(context).size.height * 1,
+                width: MediaQuery.of(context).size.width * 1),
+          ],
+        ),
+      ),
+    );
+  }
+}
